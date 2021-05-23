a4177427d15ddf9d48a8cf4737a41747af4497ef
J. Francisco R. Montufar <J.Francisco.X.R.Montufar.-ND@disney.com>
2021-05-23 11:39:42 -0500

    Picker: implemented lib to pick images an use camera

--- app/src/main/java/com/francisco/whatsapptest/ui/CompleteInformationFragment.kt
+++ app/src/main/java/com/francisco/whatsapptest/ui/CompleteInformationFragment.kt
@@ -1,21 +1,27 @@
 package com.francisco.whatsapptest.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.francisco.whatsapptest.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.francisco.whatsapptest.WhatsApp
 import com.francisco.whatsapptest.databinding.FragmentCompleteInformationBinding
 import com.francisco.whatsapptest.di.CompleteInformationComponent
 import com.francisco.whatsapptest.di.CompleteInformationModule
-import com.francisco.whatsapptest.presentation.CodeVerificationViewModel
 import com.francisco.whatsapptest.presentation.CompleteInformationViewModel
 import com.francisco.whatsapptest.util.Event
-import com.francisco.whatsapptest.util.SuccessCode
 import com.francisco.whatsapptest.util.getViewModel
+import com.fxn.pix.Options
+import com.fxn.pix.Pix
+import com.fxn.utility.PermUtil
+import java.io.File
 
 class CompleteInformationFragment : Fragment() {
 
@@ -25,6 +31,9 @@ class CompleteInformationFragment : Fragment() {
     private val completeInformationViewModel: CompleteInformationViewModel by lazy {
         getViewModel { completeInformationComponent.completeInformationViewModel }
     }
+    private val mReturnValues = arrayListOf<String>()
+    lateinit var mOptions: Options
+    lateinit var mImageFile: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,7 +53,20 @@ class CompleteInformationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        onImageClickListener()
         onSaveActionButton()
+        observeProducts()
+        initOptions()
+    }
+
+    private fun onImageClickListener() {
+        binding.ivCompleteInformation.setOnClickListener {
+            startPix()
+        }
+    }
+
+    private fun observeProducts() {
+        completeInformationViewModel.event.observe(viewLifecycleOwner, Observer(::onEventListener))
     }
 
     private fun onEventListener(event: Event<CompleteInformationViewModel.CompleteInformationNavigation>) {
@@ -63,6 +85,59 @@ class CompleteInformationFragment : Fragment() {
     private fun toGoMessages() {
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == 100) {
+            data?.let {
+                val returnValue = it.getStringArrayListExtra(Pix.IMAGE_RESULTS)
+                returnValue?.let { value ->
+                    mImageFile = File(value[0])
+                    binding.ivCompleteInformation.setImageBitmap(BitmapFactory.decodeFile(mImageFile.absolutePath))
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Pix.start(requireActivity(), mOptions);
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        "Approve permissions to open Pix ImagePicker",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun startPix() {
+        Pix.start(this, mOptions)
+    }
+
+    private fun initOptions() {
+        mOptions = Options.init()
+            .setRequestCode(100)
+            .setCount(1)
+            .setFrontfacing(true)
+            .setPreSelectedUrls(mReturnValues)
+            .setSpanCount(4)
+            .setMode(Options.Mode.Picture)
+            .setVideoDurationLimitinSeconds(0)
+            .setScreenOrientation(Options.SCREEN_ORIENTATION_LANDSCAPE)
+            .setPath("/pix/images")
+    }
+
 
     private fun onSaveActionButton() {
         binding.btCompleteInformation.setOnClickListener {
